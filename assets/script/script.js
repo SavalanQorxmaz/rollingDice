766dab4b3d254b0c7bf4a5213ab94c376ff3e573
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2023-02-03 16:55:22 +0400

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+
+
+
+
+let urlimg = location.href + 'img/'
+console.log(urlimg)
+console.log(location.href)
+
+const image = document.getElementById("image");
+console.log(image.getAttribute("src"))
+
+// image.setAttribute("src", "/assets/img/2.jpg") 
